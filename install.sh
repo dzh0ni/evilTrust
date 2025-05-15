72e0a86474012586aa683a77d9b6bfd1e3606575
Jony Rivera <jrsepulveda1988@gmail.com>
2025-05-15 01:37:25 -0400

    Update Script

--- install.sh
+++ install.sh
@@ -16,7 +16,6 @@ sudo rm -rf /usr/local/bin/eviltrust
 sudo rm -rf /usr/local/bin/eviltrustg
 sudo mv eviltrust /usr/local/bin/
 sudo mv eviltrustg /usr/local/bin/
-cd
 
 sudo apt-get -y update
 sudo apt-get install -y php
@@ -27,4 +26,5 @@ sudo mkdir -p /usr/local/share/evilTrust/images
 sudo cp images/evil.png /usr/local/share/evilTrust/images/
 sudo chmod 644 /usr/local/share/evilTrust/images/evil.png
 
-sudo wget https://raw.githubusercontent.com/dzh0ni/evilTrust/master/Install/crear_evilTrust_desktop.sh -O - | sudo bash && sudo rm -rf wget-log*
+sudo wget https://raw.githubusercontent.com/dzh0ni/evilTrust/master/Install/crear_evilTrust_desktop.sh -O - | sudo bash
+
