19af2177f58ed6a7108430be942dae7513f521c2
Jony Rivera <jrsepulveda1988@gmail.com>
2025-06-27 00:07:16 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## :computer: Instalaci�n
+## :computer: Instalación
 
 cd /opt
 sudo rm -rf evilTrust
@@ -35,13 +35,13 @@ sudo mkdir -p /usr/local/share/evilTrust/images
 sudo cp /opt/evilTrust/images/evil.png /usr/local/share/evilTrust/images/
 sudo chmod 644 /usr/local/share/evilTrust/images/evil.png
 
-# Nombre de la aplicación
+# Nombre de la aplicaciÃ³n
 APP_NAME="evilTrust"
 
 # Ruta del ejecutable
 EXEC_PATH="/usr/share/kali-menu/exec-in-shell \"evilTrust\""
 
-# Ruta del ícono
+# Ruta del Ã­cono
 ICON_PATH="/usr/local/share/evilTrust/images/evil.png"
 
 # Rutas de los archivos .desktop
@@ -61,12 +61,12 @@ Type=Application
 Categories=06-wireless-attacks;
 X-Kali-Package=wifite"
 
-# Función para crear el archivo .desktop
+# FunciÃ³n para crear el archivo .desktop
 create_desktop_file() {
     local file_path="$1"
     echo "$DESKTOP_ENTRY" | sudo tee "$file_path" > /dev/null
     sudo chmod 644 "$file_path"
-    echo "¡El archivo .desktop en $file_path se ha creado exitosamente!"
+    echo "Â¡El archivo .desktop en $file_path se ha creado exitosamente!"
 }
 
 # Crear el archivo .desktop en /usr/share/applications
@@ -75,10 +75,10 @@ create_desktop_file "$APPLICATIONS_DESKTOP_FILE"
 # Crear el archivo .desktop en /usr/share/kali-menu/applications
 create_desktop_file "$KALI_MENU_DESKTOP_FILE"
 
-# Actualizar la base de datos del menú para reflejar los cambios
+# Actualizar la base de datos del menÃº para reflejar los cambios
 sudo update-desktop-database
 sudo updatedb
 
-echo "Todos los archivos .desktop han sido creados y la base de datos del menú ha sido actualizada."
+echo "Todos los archivos .desktop han sido creados y la base de datos del menÃº ha sido actualizada."
 
 
